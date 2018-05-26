e13e58fc56044a663001fc13a0a2465ceb8afeec
Rin505 <naken505@gmail.com>
2018-05-26 15:16:15 +0300

    new commit

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,28 +3,34 @@ import React, {Component} from 'react';
  class Article extends Component {
 
         state = {
-            isOpen: true
+            isOpen: false
         };
 
     render() {
         const {article} = this.props;
         console.log(this.props);
-        const body = this.state.isOpen && <section>{article.text}</section>;
+        const body = this.state.isOpen && <section className="card-text">{article.text}</section>;
 
-        return (
-            <div>
-                <h2>{article.title}</h2>
-                <button onClick={this.handleClick}>
-                    {this.state.isOpen ? 'close' : 'open'}
-                </button>
-                {body}
-                <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
+        return <div className="card mx-auto" style={{width: '50%'}}>
+            <div className="card-header">
+                <h2>
+                    {article.title}
+                    <button onClick={this.handleClick} className="btn btn-primary btn-lg float-lg-right">
+                        {this.state.isOpen ? 'close' : 'open'}
+                    </button>
+                </h2>
             </div>
-        )
+            <div className="card-body">
+                <h6 className="card-subtitle text-muted">
+                    creation date: {(new Date(article.date)).toDateString()}
+                </h6>
+                {body}
+            </div>
+        </div>
     }
 
      handleClick = () => {
-        console.log('clicked');
+         console.log('clicked');
          this.setState({
              isOpen: !this.state.isOpen
          })
