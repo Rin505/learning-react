ca299a3fc5fcc9e4f78cd1b76b7a1779999fc02c
Rin505 <naken505@gmail.com>
2018-05-26 08:26:00 +0300

    new commit

---
+++ src/fixtures.js
@@ -0,0 +1,80 @@
+export default [
+    {
+        "id": "56aljfladsjflasdfjasdlkfj",
+        "date": "2016-06-09t15:03:23.000Z",
+        "title": "Top 30 books to read",
+        "text" : "First is:  Точка обмана " +
+        "Second is: " +
+        "Third is: " +
+        "Fourth is: ",
+        "comments": [
+            {
+                "id" : "wqerqwe",
+                "user" : "Rin",
+                "text": "I like this top , you know"
+            },
+            {
+                "id" : "qwerqwerq",
+                "user": "Naken",
+                "text": "That is a good experience , i try to read them too"
+            },
+            {
+                "id": "qerqwerwer",
+                "user": "Zamyla",
+                "text": "Good top to echieve a good results in psychology genre!!"
+            }
+        ]
+    },
+    {
+        "id": "56aljfladsjflasdfjasdlkfj",
+        "date": "2016-06-09t15:03:23.000Z",
+        "title": "Top 30 books to read",
+        "text" : "First is:  Точка обмана " +
+            "Second is: " +
+            "Third is: " +
+            "Fourth is: ",
+        "comments": [
+            {
+                "id" : "wqerqwe",
+                "user" : "Rin",
+                "text": "I like this top , you know"
+            },
+            {
+                "id" : "qwerqwerq",
+                "user": "Naken",
+                "text": "That is a good experience , i try to read them too"
+            },
+            {
+                "id": "qerqwerwer",
+                "user": "Zamyla",
+                "text": "Good top to echieve a good results in psychology genre!!"
+            }
+        ]
+    },
+    {
+        "id": "56aljfladsjflasdfjasdlkfj",
+        "date": "2016-06-09t15:03:23.000Z",
+        "title": "Top 30 books to read",
+        "text" : "First is:  Точка обмана " +
+            "Second is: " +
+            "Third is: " +
+            "Fourth is: ",
+        "comments": [
+            {
+                "id" : "wqerqwe",
+                "user" : "Rin",
+                "text": "I like this top , you know"
+            },
+            {
+                "id" : "qwerqwerq",
+                "user": "Naken",
+                "text": "That is a good experience , i try to read them too"
+            },
+            {
+                "id": "qerqwerwer",
+                "user": "Zamyla",
+                "text": "Good top to echieve a good results in psychology genre!!"
+            }
+        ]
+    }
+]
